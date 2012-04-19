ba1540b783a1c21a139eadb24945159791ce2848
Brook Riggio <brooke.riggio@gmail.com>
2012-04-19 13:13:24 -0700

    Intial config, now reading ENV for db settings.

--- wp-config.php
+++ wp-config.php
@@ -14,18 +14,16 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
-
-/** MySQL database username */
-define('DB_USER', 'username_here');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+if (isset($_SERVER["DATABASE_URL"])) {
+ $db = parse_url($_SERVER["DATABASE_URL"]);
+ define("DB_NAME", trim($db["path"],"/"));
+ define("DB_USER", $db["user"]);
+ define("DB_PASSWORD", $db["pass"]);
+ define("DB_HOST", $db["host"]);
+}
+else {
+ die("Your heroku DATABASE_URL does not appear to be correctly specified.");
+}
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -42,16 +40,20 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
+define('AUTH_KEY',         '#={Q(Yz7A{jt;U[Dhju<;2sFQ/b|,PFLa_J=&FO51huvi;|o=.^|mIe>`R>(*A |');
+define('SECURE_AUTH_KEY',  'ddwGH~VO2Wo-[8Knt.2%u]bdWMhh]o-Z]&L@_K|K*Q6nxCX5mtI)jMDR/0<s;Y@d');
+define('LOGGED_IN_KEY',    'I=BOXht.OK;btFz=F@faD:YCf6vpQXIx> [Q -bB]2mwbok&<p,C4/9HY(5%3p1L');
+define('NONCE_KEY',        '-~3;9~YO QpY/T7[.,{uB;:_Tnz:@P@[VaDBUv@u7U!$]j>~v7ncY`~qmo-;pDcI');
+define('AUTH_SALT',        ',Jxe:^`7V*UcKYx~ e:h69hScL[ofv,82K/46-]`{$Vo=S.<]YN<]Ga|c/ma]}x<');
+define('SECURE_AUTH_SALT', 'IUR-p-fdwirbJ]z*SwG$Sp1S TXvjLHL^zvy~gsbKV2Oo=i;M`|E:UVa|HWM6j4,');
+define('LOGGED_IN_SALT',   'l-H7-V*H97#eE%?<mJrAt/v3g6k`fOydCgR1Urv*,@HfsL;0B$Z+`v;t0.j4|d3_');
+define('NONCE_SALT',       'o/RBjrlc^%/|s!J9&!o8U3^Zgy?p6+HB7TH&^Y_6Txi|*vs<+R(]nVn__pvOv-$M');
 /**#@-*/
+
+/**
+ * Dynamic site name, so we can use the same config in dev and production.
+ */
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] );
 
 /**
  * WordPress Database Table prefix.
